f81f518442e958a95ad73c40fd8e84e2bd02c7ae
Jovana Bulatovic <bulatovicj07@gmail.com>
2022-02-07 18:38:16 +0100

    Izmene nakon reveiw-a code.

--- app/src/main/java/com/pma/weatherapp/ui/weather_current/WeatherCurrentFragment.kt
+++ app/src/main/java/com/pma/weatherapp/ui/weather_current/WeatherCurrentFragment.kt
@@ -11,7 +11,6 @@ class WeatherCurrentFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
